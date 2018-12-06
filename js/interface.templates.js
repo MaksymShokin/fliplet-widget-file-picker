41b6bf6b68e546b2136090016a33fcc68f3c5691
Angel Nikolov <angel.nikolov@mentormate.com>
2018-12-06 11:27:55 +0200

    Added audio

--- js/interface.templates.js
+++ js/interface.templates.js
@@ -12,6 +12,22 @@ this["Fliplet"]["Widget"]["Templates"]["templates.app"] = Handlebars.template({"
     + "</div>\n</div>";
 },"useData":true});
 
+this["Fliplet"]["Widget"]["Templates"]["templates.audio"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    return " selected ";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<div class=\"col-xs-4 col-sm-3 col-md-2 item-holder image audio "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.selected : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "\" data-file-id=\""
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\"\n  data-file-ext=\""
+    + alias4(((helper = (helper = helpers.ext || (depth0 != null ? depth0.ext : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ext","hash":{},"data":data}) : helper)))
+    + "\">\n  <div class=\"thumb-wrapper\">\n    <div class=\"btn-group more-options\">\n      <button class=\"btn btn-link\" type=\"button\" id=\"\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n        <i class=\"fa fa-ellipsis-v\"></i>\n      </button>\n      <ul class=\"dropdown-menu dropdown-menu-right\">\n        <li class=\"text-danger delete-file\">Delete</li>\n      </ul>\n    </div>\n    <div class=\"image-holder file\">\n      <div class=\"image-overlay\" style=\"opacity: 1\">\n        <i class=\"fa fa-audio\" aria-hidden=\"true\"></i>\n      </div>\n    </div>\n  </div>\n  <div class=\"image-title\">"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</div>\n</div>";
+},"useData":true});
+
 this["Fliplet"]["Widget"]["Templates"]["templates.document"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     return " selected ";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
